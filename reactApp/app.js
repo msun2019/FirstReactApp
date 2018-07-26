2bd27d3412544ac5cecd0a0cd156797f180f24ce
Michael Sun <msun2019@pingry.org>
2018-07-26 11:29:19 -0700

    added InputLine component

--- reactApp/app.js
+++ reactApp/app.js
@@ -7,19 +7,51 @@ let dummyData = ["Wash dishes", "Do laundry"];
 class TodoList extends React.Component{
   render(){
     return (
-      <ul>
-        {dummyData.map((todo) => <Todo task={todo}/>)}
-      </ul>
+      <div>
+        <InputLine/>
+        <ul>
+          {dummyData.map((todo) => <Todo task={todo}/>)}
+        </ul>
+      </div>
+
     )
   }
 }
 class Todo extends React.Component{
   render(){
-    console.log(this.props.task)
     return (
-
       <li><button>X</button>{this.props.task}</li>
     )
+  }
+}
+class InputLine extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      task: ''
+    }
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    alert()
+  }
+  handleChange(e){
+    this.setState({
+      task: e.target.value
+    })
+  }
+  render(){
+    return (
+    <div>
+      <form onSubmit={(e)=>this.handleSubmit(e)}>
+        <input type="text"
+          placeholder="New Task"
+          onChange={(e)=>this.handleChange(e)}
+        />
+        <button type='submit'>Add todo</button>
+      </form>
+    </div>
+  )
   }
 }
 ReactDom.render(<TodoList/>, document.getElementById('root'))
